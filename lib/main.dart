fc4c7e277d55d81127eafbe69edcb34f368abf14
Ênio Viana <eniocc@gmail.com>
2020-02-19 23:08:10 -0300

    Classe modelo

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,52 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+main() => runApp(ExpensesApp());
 
-class MyApp extends StatelessWidget {
+final _transactions = [
+  Transaction(
+    id: 't1',
+    title: 'Novo tênis d corrida',
+    value: 100.00,
+    date: DateTime.now(),
+  ),
+  Transaction(
+    id: 't2',
+    title: 'Conta de luz',
+    value: 110.00,
+    date: DateTime.now(),
+  ),
+];
+
+class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(home: MyHome());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Despesas pessoais'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+          ),
+          Card(
+            child: Text('Lista de transações'),
+          )
+        ],
       ),
     );
   }

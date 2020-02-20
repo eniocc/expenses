84f15cdbde5a7899c856e2ccc76677c40167752f
Ênio Viana <eniocc@gmail.com>
2020-02-20 00:11:50 -0300

    Dividindo o App em Widgets

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,40 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+import './transaction_form.dart';
+import './transaction_list.dart';
+
+class TransactionUser extends StatefulWidget {
+  @override
+  _TransactionUserState createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tênis d corrida',
+      value: 100.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 110.89,
+      date: DateTime.now(),
+    ),
+  ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransactionList(_transactions),
+        TransactionForm(),
+      ],
+      
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,14 @@
-import 'package:expenses/models/transaction.dart';
+import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
-
-final _transactions = [
-  Transaction(
-    id: 't1',
-    title: 'Novo tênis d corrida',
-    value: 100.00,
-    date: DateTime.now(),
-  ),
-  Transaction(
-    id: 't2',
-    title: 'Conta de luz',
-    value: 110.89,
-    date: DateTime.now(),
-  ),
-];
 
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHome());
+    return MaterialApp(
+      home: MyHome(),
+    );
   }
 }
 
@@ -34,7 +20,7 @@ class MyHome extends StatelessWidget {
         title: Text('Despesas pessoais'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -44,54 +30,7 @@ class MyHome extends StatelessWidget {
               child: Text('Gráfico'),
             ),
           ),
-          Column(
-            children: _transactions.map((tr){
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin:  EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 2
-                        )
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(                        
-                        'R\$ ${tr.value.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          tr.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold
-                          ),
-                          ),
-                        Text(
-                          DateFormat('d-MMM-y').format(tr.date),
-                          style: TextStyle(
-                            color: Colors.black54
-                          ),)
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-          )
+          TransactionUser(),
         ],
       ),
     );
